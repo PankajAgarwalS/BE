e4c95f0153fc3ae0b24be2775ba98b0c88ebbc61
Pankaj Agarwal <98219223+PankajAgarwalS@users.noreply.github.com>
2024-11-09 18:26:30 +0530

    Update Huff.py

--- DAA/Huff.py
+++ DAA/Huff.py
@@ -1,48 +1,63 @@
+import heapq
+
+# Node class for Huffman Tree
 class Node:
-    def __init__(self, freq=0, symbol='', left=None, right=None, is_leaf=False):
+    def __init__(self, freq, symbol='', left=None, right=None):
         self.freq = freq
         self.symbol = symbol
         self.left = left
         self.right = right
-        self.is_leaf = is_leaf
 
-    def create_leaf(self, symbol, freq):
-        self.is_leaf = True
-        self.symbol = symbol
-        self.freq = freq
+    # For heap comparisons
+    def __lt__(self, other):
+        return self.freq < other.freq
 
-    def create_node(self, freq, left, right):
-        self.freq = freq
-        self.left = left
-        self.right = right
+# Function to generate Huffman codes
+def generate_huffman_codes(node, code='', code_map={}):
+    if node:
+        if not node.left and not node.right:  # Leaf node
+            code_map[node.symbol] = code
+        generate_huffman_codes(node.left, code + '0', code_map)
+        generate_huffman_codes(node.right, code + '1', code_map)
+    return code_map
 
-def insert_node(arr, node):
-    arr.append(node)
-    return sorted(arr, key=lambda x: x.freq)
-
-def generate_huffman(node, code, code_map):
-    if node.is_leaf:
-        code_map[node.symbol] = code
-    else:
-        generate_huffman(node.left, code + '0', code_map)
-        generate_huffman(node.right, code + '1', code_map)
-
-def main():
-    s = input("Enter the string: ")
-    freq_map = {i: s.count(i) for i in set(s)}
+# Main function to perform Huffman Encoding
+def huffman_encoding(s):
+    # Frequency map of characters
+    freq_map = {char: s.count(char) for char in set(s)}
     print("Frequency:", freq_map)
-    
-    nodes = [Node().create_leaf(sym, freq) or Node(freq, sym) for sym, freq in freq_map.items()]
-    while len(nodes) > 1:
-        n1, n2 = nodes.pop(0), nodes.pop(0)
-        new_node = Node(n1.freq + n2.freq, left=n1, right=n2)
-        nodes = insert_node(nodes, new_node)
-    
-    code_map = {}
-    generate_huffman(nodes[0], '', code_map)
-    
+
+    # Priority queue (min-heap) for nodes
+    heap = [Node(freq, symbol) for symbol, freq in freq_map.items()]
+    heapq.heapify(heap)
+
+    # Build Huffman Tree using a greedy strategy
+    while len(heap) > 1:
+        n1 = heapq.heappop(heap)
+        n2 = heapq.heappop(heap)
+        merged_node = Node(n1.freq + n2.freq, left=n1, right=n2)
+        heapq.heappush(heap, merged_node)
+
+    # Generate Huffman codes
+    code_map = generate_huffman_codes(heap[0])
     print("Huffman Codes:", code_map)
-    encoded_string = ''.join(code_map[i] + ' ' for i in s)
+
+    # Encode the input string
+    encoded_string = ''.join(code_map[char] for char in s)
     print("Encoded String:", encoded_string)
 
-main()
+# Input string for encoding
+s = input("Enter the string: ")
+huffman_encoding(s)
+
+
+
+"""
+ the time complexity of Huffman Encoding is Huffman Encoding is a lossless compression algorithm that assigns shorter codes to more frequent characters to minimize data size. The program:
+
+Counts character frequencies.
+Builds a Huffman Tree using a greedy strategy.
+Generates unique binary codes for each character.
+Encodes the input string using these codesO(nlogn), where n is the length of the input string.
+
+"""
